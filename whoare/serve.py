8b1af6e44c9420044b6d81594f66eddda89360c8
Andres Vazquez <andres@data99.com.ar>
2020-10-21 21:00:56 -0300

    preparing an entry point

--- whoare/serve.py
+++ whoare/serve.py
@@ -1,6 +1,7 @@
 """
 Iterate over priority domains and send data to server
 """
+import argparse
 import logging
 from time import sleep
 import requests
@@ -9,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 class WhoAreShare:
-    def __init__(self, get_domains_url, post_url, token, torify=True, pause_between_calls=20):
+    def __init__(self, get_domains_url, post_url, token, torify=True, pause_between_calls=41):
         self.torify = torify  # use local IP and also torify
         self.post_url = post_url  # destination URL to share data (will be processed outside)
         self.token = token
@@ -18,8 +19,12 @@ class WhoAreShare:
 
     def run(self):
         """ get domains and _whois_ them """
+        logger.info('Start runing')
         while True:
             domain = self.get_one()
+            logger.info(f'Domain {domain}')
+            print(domain)
+            raise
             wa = WhoAre()
             raw = wa.get_raw(domain)
             self.post_one(raw)
@@ -27,6 +32,7 @@ class WhoAreShare:
             # if torify start a second queue
             if self.torify:
                 domain = self.get_one()
+                logger.info(f'Domain {domain} torify')
                 wa = WhoAre()
                 raw = wa.get_raw(domain, torify=True)
                 self.post_one(raw)
@@ -37,16 +43,44 @@ class WhoAreShare:
         logger.info('Getting one')
         headers = {'Authorization': f'Token {self.token}'}
         response = requests.get(self.get_domains_url, headers=headers)
-        jresponse = response.json()
+        if response.status_code != 200:
+            raise ValueError(f'Error GET status {response.status_code}: {response.text}')
+        
+        try:
+            jresponse = response.json()
+        except Exception:
+            print(f'ERROR parsing {response.text}')
+            raise
+        
         logger.info(f' - Get {jresponse}')
-        return jresponse['domain']
+        
+        return jresponse[0]['domain']
     
     def post_one(self, raw):
         logger.info('POST one')
         headers = {'Authorization': f'Token {self.token}'}
         data = {'raw': raw}
-        response = requests.post(self.post_url, data=data headers=headers)
+        response = requests.post(self.post_url, data=data, headers=headers)
         jresponse = response.json()
         logger.info(f' - POST {jresponse}')
         return jresponse['ok']
+
+def main():
+    parser = argparse.ArgumentParser(prog='whoare-share')
+    parser.add_argument('--get', nargs='?', help='URL to get domains from', type=str, default='https://nic.opendatacordoba.org/api/v1/dominios/next-priority/')
+    parser.add_argument('--post', nargs='?', help='URL to post results to', type=str, required=True)
+    parser.add_argument('--token', nargs='?', help='Token to use as Header Autorization', type=str, required=True)
+    parser.add_argument('--torify', nargs='?', type=bool, default=True, help='Use torify for WhoIs command')
+    parser.add_argument('--pause', nargs='?', help='Pause between calls', default=41, type=int)
     
+    args = parser.parse_args()
+
+    was = WhoAreShare(
+        get_domains_url=args.get,
+        post_url=args.post,
+        token=args.token,
+        torify=args.torify,
+        pause_between_calls=args.pause
+    )
+
+    was.run()
