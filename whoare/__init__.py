02b5b9429e89d1ec1f09943120ab1ac04d067701
Andres Vazquez <andres@data99.com.ar>
2020-10-22 23:59:14 -0300

    fix travis

--- whoare/__init__.py
+++ whoare/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '0.1.29'
+__version__ = '0.1.30'
+
+# env:
+#   global:
+#     - PYPI_VERSION=wget -qO- https://pypi.org/pypi/whoare/json | jq '.info.version'
+#     - HERE_VERSION=echo -e "from whoare import __version__\nprint(f'\"{__version__}\"')" | python
