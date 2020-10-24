a89cb080b306430fed9beb4d5f3230d02526d023
Andres Vazquez <andres@data99.com.ar>
2020-10-24 12:33:09 -0300

    fix command

--- test_require_update.sh
+++ test_require_update.sh
@@ -1,9 +1,14 @@
 PYPI_VERSION=$(wget -qO- https://pypi.org/pypi/whoare/json | jq '.info.version')
-HERE_VERSION=$(echo -e "from whoare import __version__\nprint(f'\"{__version__}\"')" | python)
+echo "Pypi version found: $PYPI_VERSION"
 
-if [$PYPI_VERSION == $HERE_VERSION] 
+HERE_VERSION=$(echo -e "from whoare import __version__\nprint(f'\"{__version__}\"')" | python)
+echo "Current version: $HERE_VERSION"
+
+if [ $PYPI_VERSION == $HERE_VERSION ]
 then 
     REQUIRE_PYPI_UPDATE=false
 else
     REQUIRE_PYPI_UPDATE=true
 fi
+
+echo "Require update: $REQUIRE_PYPI_UPDATE"
